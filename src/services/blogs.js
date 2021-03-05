1d2a6e33ffc695d1f6937c4acac57ef6121df06f
incredible92 <sulymanabdullahi@gmail.com>
2021-03-05 16:58:15 +0000

    add: blog

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
+
+
+const createBlog = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+}
+
 
 export default { getAll }
